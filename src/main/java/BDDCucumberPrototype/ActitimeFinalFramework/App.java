45e5711668b19aa3cc9248d2255fcdc7b9feff46
gopal <gopalakkatangerhal1993@gmail.com>
2024-01-15 11:37:41 +0530

    first round of commit...

---
+++ src/main/java/BDDCucumberPrototype/ActitimeFinalFramework/App.java
@@ -0,0 +1,13 @@
+package BDDCucumberPrototype.ActitimeFinalFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
